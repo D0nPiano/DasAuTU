bea4c2de40ac46ab8d4d337f024bcee9f4482ba8
D0nPiano <spam-mails@abwesend.de>
2016-12-13 16:47:32 +0100

    Added Classes for main and RundkursController Main is now modeController Rundkurs Class is defined in header. Didn't work otherwise somehow

---
+++ autu_control/include/autu_control/AutoController.h
@@ -0,0 +1,6 @@
+#pragma once
+
+class AutoController {
+  public:
+    virtual void run() = 0; // Pure virtual function makes
+};
--- autu_control/include/autu_control/rundkursController.h
+++ autu_control/include/autu_control/rundkursController.h
@@ -1,3 +1,29 @@
 #pragma once
 
+#include "autu_control/AutoController.h"
+#include "ros/ros.h"
+#include "std_msgs/String.h"
 
+class RundkursController : public AutoController  {
+   public:
+   		RundkursController(ros::NodeHandle * n);
+    	void run();
+   private:
+   		void simpleController();
+   		ros::NodeHandle * n;
+   		ros::Subscriber modeSubscriber;
+};
+
+RundkursController::RundkursController(ros::NodeHandle * n):
+	n(n)
+	{
+	ROS_INFO("New RundkursController");
+}
+
+void RundkursController::simpleController(){
+  ROS_INFO("test - XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX - ******************");
+}
+
+void RundkursController::run () {
+	this->simpleController();
+}
