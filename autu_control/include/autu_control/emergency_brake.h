f11309dfadd40a0db1e38edaf24cc8ed2d41f4d6
Hendrik Göttmann <hendrikgoettmann@gmail.com>
2016-12-23 09:51:41 +0100

    GridCell für Baselink wird gefunden

--- autu_control/include/autu_control/emergency_brake.h
+++ autu_control/include/autu_control/emergency_brake.h
@@ -1,20 +1,38 @@
 #ifndef EMERGENCYBRAKE_H
 #define EMERGENCYBRAKE_H
 
+#include <vector>
+
 #include "nav_msgs/OccupancyGrid.h"
+#include "nav_msgs/Odometry.h"
 #include "ros/ros.h"
 
+#include "pses_basis/CarInfo.h"
 #include "pses_basis/Command.h"
 
 class EmergencyBrake {
 public:
   EmergencyBrake(ros::NodeHandle *n);
+  ~EmergencyBrake();
   void gridMapCallback(const nav_msgs::OccupancyGridConstPtr &msg);
   void commandCallback(const pses_basis::CommandConstPtr &cmd);
+  void speedCallback(const pses_basis::CarInfoConstPtr &msg);
+  void odomCallback(const nav_msgs::OdometryConstPtr &msg);
+  void timerCallback(const ros::TimerEvent &);
 
 private:
   ros::Publisher command_pub;
+  ros::Publisher map_pub;
+  ros::Subscriber command_sub;
+  ros::Subscriber speed_sub;
+  ros::Subscriber map_sub;
+  ros::Subscriber odom_sub;
+  ros::Timer timer;
   int16_t maxMotorLevel;
+  float currentSpeed;
+  nav_msgs::OccupancyGrid grid;
+  uint32_t gridId;
+  double carX, carY;
 };
 
 #endif // EMERGENCYBRAKE_H
--- autu_control/src/emergency_brake.cpp
+++ autu_control/src/emergency_brake.cpp
@@ -1,11 +1,42 @@
 #include "autu_control/emergency_brake.h"
 
-EmergencyBrake::EmergencyBrake(ros::NodeHandle *n) : maxMotorLevel(13) {
+EmergencyBrake::EmergencyBrake(ros::NodeHandle *n)
+    : maxMotorLevel(999), gridId(0), carX(0), carY(0) {
   command_pub = n->advertise<pses_basis::Command>("pses_basis/command", 10);
+
+  map_pub = n->advertise<nav_msgs::OccupancyGrid>("autu/emergency_map", 10);
+
+  timer = n->createTimer(
+      ros::Duration(1.0),
+      std::bind(&EmergencyBrake::timerCallback, this, std::placeholders::_1));
+
+  command_sub = n->subscribe<pses_basis::Command>(
+      "autu/command", 10,
+      std::bind(&EmergencyBrake::commandCallback, this, std::placeholders::_1));
+
+  speed_sub = n->subscribe<pses_basis::CarInfo>(
+      "pses_basis/car_info", 10,
+      std::bind(&EmergencyBrake::speedCallback, this, std::placeholders::_1));
+
+  map_sub = n->subscribe<nav_msgs::OccupancyGrid>(
+      "autu/gridmap", 10,
+      std::bind(&EmergencyBrake::gridMapCallback, this, std::placeholders::_1));
+
+  odom_sub = n->subscribe<nav_msgs::Odometry>(
+      "/odom", 10,
+      std::bind(&EmergencyBrake::odomCallback, this, std::placeholders::_1));
 }
 
+EmergencyBrake::~EmergencyBrake() { command_sub.shutdown(); }
+
 void EmergencyBrake::gridMapCallback(
-    const nav_msgs::OccupancyGridConstPtr &msg) {}
+    const nav_msgs::OccupancyGridConstPtr &msg) {
+  grid = *msg;
+  //  for (int x = 0; x < mapInfo.width; ++x)
+  // for (int y = 0; y < mapInfo.height; ++y)
+  // grid.data[x * mapInfo.height + 2000] = 100;
+  // map_pub.publish(grid);
+}
 
 void EmergencyBrake::commandCallback(const pses_basis::CommandConstPtr &cmd) {
   // copy message to avoid crazy shit
@@ -19,15 +50,43 @@ void EmergencyBrake::commandCallback(const pses_basis::CommandConstPtr &cmd) {
   command_pub.publish(replacement);
 }
 
+void EmergencyBrake::speedCallback(const pses_basis::CarInfoConstPtr &msg) {
+  currentSpeed = msg->speed;
+}
+
+void EmergencyBrake::odomCallback(const nav_msgs::OdometryConstPtr &msg) {
+  carX = msg->pose.pose.position.x;
+  carY = msg->pose.pose.position.y;
+}
+
+void EmergencyBrake::timerCallback(const ros::TimerEvent &) {
+  ROS_INFO("Timer Callback");
+  int x = (carX - grid.info.origin.position.x) / grid.info.resolution;
+  int y = (carY - grid.info.origin.position.y) / grid.info.resolution;
+  // check array bounds
+  ROS_INFO("x: %d y: %d", x, y);
+  //  grid.data[y * grid.info.width + x] = 100;
+
+  nav_msgs::OccupancyGrid debugGrid = grid;
+  /*debugGrid.header.seq = gridId++;
+  debugGrid.header.stamp = ros::Time::now();
+  debugGrid.header.frame_id = "map";
+  debugGrid.info.resolution = 0.05;
+  debugGrid.info.height = 50;
+  debugGrid.info.width = 100;
+  debugGrid.info.origin.orientation.w = 1;*/
+  // for (uint32_t i = 0; i < debugGrid.info.width * debugGrid.info.height; ++i)
+  const int pos = y * debugGrid.info.width + x;
+  if (pos < debugGrid.data.size())
+    debugGrid.data[pos] = 100;
+  map_pub.publish(debugGrid);
+}
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "emergency_brake");
   ros::NodeHandle n;
   EmergencyBrake emergencyBrake(&n);
   ROS_INFO("Emergency Brake launched");
-
-  ros::Subscriber command_sub = n.subscribe<pses_basis::Command>(
-      "autu/command", 10, std::bind(&EmergencyBrake::commandCallback,
-                                    emergencyBrake, std::placeholders::_1));
 
   ros::spin();
   return 0;
