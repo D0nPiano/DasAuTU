b4a48e5c1f24fd1652b5e208a84cd2b3b746f049
Hendrik Göttmann <hendrikgoettmann@gmail.com>
2016-12-26 17:10:44 +0100

    EmergencyBrake: Code aufgeräumt
    Bereit für ersten Test auf dem Auto

--- autu_control/include/autu_control/emergency_brake.h
+++ autu_control/include/autu_control/emergency_brake.h
@@ -1,13 +1,9 @@
 #ifndef EMERGENCYBRAKE_H
 #define EMERGENCYBRAKE_H
 
-#include <vector>
-
-#include "nav_msgs/OccupancyGrid.h"
 #include "nav_msgs/Odometry.h"
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
-#include <tf/transform_listener.h>
 
 #include "pses_basis/CarInfo.h"
 #include "pses_basis/Command.h"
@@ -16,31 +12,25 @@ class EmergencyBrake {
 public:
   EmergencyBrake(ros::NodeHandle *n);
   ~EmergencyBrake();
-  void gridMapCallback(const nav_msgs::OccupancyGridConstPtr &msg);
   void commandCallback(const pses_basis::CommandConstPtr &cmd);
   void speedCallback(const pses_basis::CarInfoConstPtr &msg);
   void odomCallback(const nav_msgs::OdometryConstPtr &msg);
-  void occupyCell(nav_msgs::OccupancyGrid &gridmap,
-                  const geometry_msgs::Point &point);
+  void updateDistanceToObstacle();
   void laserscanCallback(const sensor_msgs::LaserScanConstPtr &msg);
   void timerCallback(const ros::TimerEvent &);
 
 private:
   ros::Publisher command_pub;
-  ros::Publisher map_pub;
   ros::Subscriber command_sub;
   ros::Subscriber speed_sub;
-  ros::Subscriber map_sub;
   ros::Subscriber odom_sub;
   ros::Subscriber laserscan_sub;
   ros::Timer timer;
   int16_t maxMotorLevel;
   float currentSpeed;
-  nav_msgs::OccupancyGridConstPtr grid;
-  tf::TransformListener tfListener;
-  uint32_t gridId;
-  double carX, carY;
   float carWidth;
+  float distanceToObstacle;
+  sensor_msgs::LaserScanConstPtr laserscan;
 };
 
 #endif // EMERGENCYBRAKE_H
--- autu_control/src/emergency_brake.cpp
+++ autu_control/src/emergency_brake.cpp
@@ -2,13 +2,11 @@
 #include <limits>
 #include <math.h>
 
-#define DURATION 1.0
+#define DURATION 0.1
 
 EmergencyBrake::EmergencyBrake(ros::NodeHandle *n)
-    : maxMotorLevel(999), gridId(0), carX(0), carY(0), carWidth(0.2) {
+    : maxMotorLevel(999), carWidth(0.2), distanceToObstacle(10.0) {
   command_pub = n->advertise<pses_basis::Command>("pses_basis/command", 10);
-
-  map_pub = n->advertise<nav_msgs::OccupancyGrid>("autu/emergency_map", 10);
 
   timer = n->createTimer(
       ros::Duration(DURATION),
@@ -26,20 +24,17 @@ EmergencyBrake::EmergencyBrake(ros::NodeHandle *n)
       "/scan", 10, std::bind(&EmergencyBrake::laserscanCallback, this,
                              std::placeholders::_1));
 
-  map_sub = n->subscribe<nav_msgs::OccupancyGrid>(
-      "autu/gridmap", 10,
-      std::bind(&EmergencyBrake::gridMapCallback, this, std::placeholders::_1));
-
-  odom_sub = n->subscribe<nav_msgs::Odometry>(
-      "/odom", 10,
-      std::bind(&EmergencyBrake::odomCallback, this, std::placeholders::_1));
+  /*  odom_sub = n->subscribe<nav_msgs::Odometry>(
+        "/odom", 10,
+        std::bind(&EmergencyBrake::odomCallback, this,
+     std::placeholders::_1));*/
 }
 
-EmergencyBrake::~EmergencyBrake() { command_sub.shutdown(); }
-
-void EmergencyBrake::gridMapCallback(
-    const nav_msgs::OccupancyGridConstPtr &msg) {
-  grid = msg;
+EmergencyBrake::~EmergencyBrake() {
+  command_sub.shutdown();
+  speed_sub.shutdown();
+  odom_sub.shutdown();
+  laserscan_sub.shutdown();
 }
 
 void EmergencyBrake::commandCallback(const pses_basis::CommandConstPtr &cmd) {
@@ -58,93 +53,45 @@ void EmergencyBrake::speedCallback(const pses_basis::CarInfoConstPtr &msg) {
   currentSpeed = msg->speed;
 }
 
-void EmergencyBrake::odomCallback(const nav_msgs::OdometryConstPtr &msg) {
-  //  carX = msg->pose.pose.position.x;
-  //  carY = msg->pose.pose.position.y;
-}
+void EmergencyBrake::odomCallback(const nav_msgs::OdometryConstPtr &msg) {}
 
-void EmergencyBrake::occupyCell(nav_msgs::OccupancyGrid &gridmap,
-                                const geometry_msgs::Point &point) {
-  int x = (point.x - gridmap.info.origin.position.x) / gridmap.info.resolution;
-  int y = (point.y - gridmap.info.origin.position.y) / gridmap.info.resolution;
-  if (0 < x && x < gridmap.info.width)
-    if (0 < y && y < gridmap.info.height) {
-      const int pos = y * gridmap.info.width + x;
-      if (pos < gridmap.data.size())
-        gridmap.data[pos] = 100;
+void EmergencyBrake::updateDistanceToObstacle() {
+  if (laserscan != nullptr) {
+    float d_min = std::numeric_limits<float>::max();
+
+    for (size_t i = 0; i < laserscan->ranges.size(); ++i) {
+      const float r = laserscan->ranges[i];
+      if (laserscan->range_min <= r && r <= laserscan->range_max) {
+        // alpha in radians and always positive
+        const float alpha =
+            std::abs(i * laserscan->angle_increment + laserscan->angle_min);
+        const float b = carWidth / 2.0;
+        const float r_max = b / std::sin(alpha);
+        if (r < r_max) {
+          // distance to obstacle
+          const float d = r * std::cos(alpha);
+          if (d < d_min)
+            d_min = d;
+        }
+      }
     }
+
+    if (d_min > laserscan->range_max)
+      d_min = laserscan->range_max;
+
+    distanceToObstacle = d_min;
+  }
 }
 
 void EmergencyBrake::laserscanCallback(
     const sensor_msgs::LaserScanConstPtr &msg) {
-  float d_min = std::numeric_limits<float>::max();
-  for (size_t i = 0; i < msg->ranges.size(); ++i) {
-    const float r = msg->ranges[i];
-    if (msg->range_min <= r && r <= msg->range_max) {
-      // alpha in radians and always positive
-      const float alpha = std::abs(i * msg->angle_increment + msg->angle_min);
-      const float sin_alpha = std::sin(alpha);
-      const float b = carWidth / 2.0;
-      const float r_max = b / sin_alpha;
-      if (r < r_max) {
-        // distance to obstacle
-        const float d = r * std::cos(alpha);
-        if (d < d_min)
-          d_min = d;
-      }
-    }
-  }
-  ROS_INFO("dist: %f", d_min);
+  laserscan = msg;
 }
 
 void EmergencyBrake::timerCallback(const ros::TimerEvent &) {
-
-  /*if (grid != nullptr) {
-
-    geometry_msgs::PointStamped frontSensor;
-    geometry_msgs::Vector3Stamped direction;
-    tf::StampedTransform transform;
-    try {
-      auto now = ros::Time::now();
-      tfListener.waitForTransform("/front_sensor", "/map", now,
-                                  ros::Duration(DURATION / 2));
-      tfListener.lookupTransform("/front_sensor", "/map", now, transform);
-
-      geometry_msgs::PointStamped frontSensorIn;
-      frontSensorIn.header.frame_id = "/front_sensor";
-      tfListener.transformPoint("/map", frontSensorIn, frontSensor);
-
-      geometry_msgs::Vector3Stamped directionIn;
-      directionIn.vector.x = 1;
-      directionIn.header.frame_id = "/front_sensor";
-      tfListener.transformVector("/map", directionIn, direction);
-    } catch (tf::TransformException &ex) {
-      ROS_ERROR("%s", ex.what());
-      return;
-    }
-    // TODO remove
-    nav_msgs::OccupancyGrid debugGrid;
-
-    debugGrid.header.seq = gridId++;
-    debugGrid.header.stamp = ros::Time::now();
-    debugGrid.header.frame_id = grid->header.frame_id;
-
-    debugGrid.info.height = 2 / grid->info.resolution;
-    debugGrid.info.width = 2 / grid->info.resolution;
-    debugGrid.info.resolution = grid->info.resolution;
-    debugGrid.info.origin.orientation.w = 1;
-
-    const uint32_t dataSize = debugGrid.info.width * debugGrid.info.height;
-    for (uint32_t i = 0; i < dataSize; ++i)
-      debugGrid.data.push_back(-1);
-
-    debugGrid.info.origin.position = frontSensor.point;
-    debugGrid.info.origin.position.x -= 1;
-    debugGrid.info.origin.position.y -= 1;
-
-    occupyCell(debugGrid, frontSensor.point);
-    map_pub.publish(debugGrid);
-}*/
+  updateDistanceToObstacle();
+  maxMotorLevel = 10 * std::sqrt(2 * distanceToObstacle);
+  ROS_INFO("dist: %f maxLevel: %d", distanceToObstacle, maxMotorLevel);
 }
 
 int main(int argc, char **argv) {
