27c9407435c40e41a8247f77ea9942ba3ca2c6e0
D0nPiano <spam-mails@abwesend.de>
2017-01-19 16:32:58 +0100

    Changed bash script to have roscore on car

--- exportIp.sh
+++ exportIp.sh
@@ -4,5 +4,5 @@ ipaddress="$(hostname -I | sed 's/\s.*$//')"
 
 export ROS_IP=$ipaddress
 export ROS_HOSTNAME="$ipaddress"
-export ROS_MASTER_URI="http://$ipaddress:11311/"
+export ROS_MASTER_URI="http://10.42.0.1:11311/"
 echo Schönen Tag noch Nummer $ipaddress!
