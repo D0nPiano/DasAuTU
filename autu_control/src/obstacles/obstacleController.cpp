1cff5ae274b8428343932dd767e9440a4def7949
Hendrik Göttmann <hendrikgoettmann@gmail.com>
2017-02-27 21:58:04 +0100

    Nur noch Probleme im Kurveninneren

--- autu_control/src/obstacles/obstacleController.cpp
+++ autu_control/src/obstacles/obstacleController.cpp
@@ -4,8 +4,7 @@
 #include "autu_control/obstacles/obstacleController.h"
 #include "ros/ros.h"
 
-Point point_new(float x, float y)
-{
+Point point_new(float x, float y) {
   Point a;
   a.x = x;
   a.y = y;
@@ -15,46 +14,49 @@ Point point_new(float x, float y)
 ObstacleController::ObstacleController(ros::NodeHandle *n,
                                        ros::Publisher *command_pub,
                                        bool startDriving)
-    : n(n), command_pub(command_pub) {
+    : n(n), command_pub(command_pub), us_front(0) {
   ROS_INFO("New ObstacleController");
 
-
-  goal_pub = n->advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1000);
+  goal_pub =
+      n->advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1000);
 
   plan_command_sub = n->subscribe<geometry_msgs::Twist>(
       "cmd_vel", 10, &ObstacleController::convertCommand, this);
 
-  if(!startDriving){
-  	ROS_INFO("Warte auf Befehle...");
+  sensorDataSub = n->subscribe<pses_basis::SensorData>(
+      "pses_basis/sensor_data", 10, &ObstacleController::sensorDataCallback,
+      this);
+
+  if (!startDriving) {
+    ROS_INFO("Warte auf Befehle...");
   } else {
-  	ROS_INFO("Suche Rundkurs, loading from XML");
-  	routeXML.LoadFile("/home/pses/route.xml");
+    ROS_INFO("Suche Rundkurs, loading from XML");
+    routeXML.LoadFile("/home/pses/route.xml");
 
-  	// save data to array
-  	tinyxml2::XMLNode * pRoot = routeXML.FirstChild();
-  	if (pRoot == nullptr){
-  		ROS_INFO("XML ist leer");
-  		return;
-  	}
+    // save data to array
+    tinyxml2::XMLNode *pRoot = routeXML.FirstChild();
+    if (pRoot == nullptr) {
+      ROS_INFO("XML ist leer");
+      return;
+    }
 
-  	tinyxml2::XMLElement * currentPointElement = pRoot->FirstChildElement("point");
-	while (currentPointElement != nullptr)
-		{
-			float currentX;
-			float currentY;
-			currentPointElement->QueryFloatAttribute("x", &currentX);
-			currentPointElement->QueryFloatAttribute("y", &currentY);
+    tinyxml2::XMLElement *currentPointElement =
+        pRoot->FirstChildElement("point");
+    while (currentPointElement != nullptr) {
+      float currentX;
+      float currentY;
+      currentPointElement->QueryFloatAttribute("x", &currentX);
+      currentPointElement->QueryFloatAttribute("y", &currentY);
 
-			Point currentPoint = point_new(currentX, currentY);
+      Point currentPoint = point_new(currentX, currentY);
 
-			points.push_back(currentPoint);
-			currentPointElement = currentPointElement->NextSiblingElement("point");
-		}
+      points.push_back(currentPoint);
+      currentPointElement = currentPointElement->NextSiblingElement("point");
+    }
 
-	for (Point i : points ) {
-  		ROS_INFO("Points: [%f], [%f]", i.x, i.y);
-	}
-
+    for (Point i : points) {
+      ROS_INFO("Points: [%f], [%f]", i.x, i.y);
+    }
   }
 
   currentGoal = -1;
@@ -68,8 +70,8 @@ ObstacleController::~ObstacleController() {
 void ObstacleController::sendNextGoal() {
   ROS_INFO("Sending Next Goal");
   currentGoal++;
-  if(currentGoal == (int) points.size()){
-  	currentGoal = 0;
+  if (currentGoal == (int)points.size()) {
+    currentGoal = 0;
   }
   geometry_msgs::PoseStamped nextGoal;
   nextGoal.pose.position.x = points[currentGoal].x;
@@ -87,47 +89,64 @@ void ObstacleController::sendNextGoal() {
   ros::spinOnce();
 }
 
-bool ObstacleController::isNearToNextGoal(const geometry_msgs::PointStamped* currentPosition) {
-	if(currentGoal == -1){
-		return true;
-	}
+bool ObstacleController::isNearToNextGoal(
+    const geometry_msgs::PointStamped *currentPosition) {
+  if (currentGoal == -1) {
+    return true;
+  }
 
-  	/*ROS_INFO("Current Position: [%f], [%f]", currentPosition->point.x, currentPosition->point.y);
-  	ROS_INFO("currentGoal: [%d]", currentGoal);
-  	ROS_INFO("Vektor X: [%f]", points[currentGoal].x);
-  	*/
+  /*ROS_INFO("Current Position: [%f], [%f]", currentPosition->point.x,
+  currentPosition->point.y);
+  ROS_INFO("currentGoal: [%d]", currentGoal);
+  ROS_INFO("Vektor X: [%f]", points[currentGoal].x);
+  */
 
-  	float diffx = pow(currentPosition->point.x - points[currentGoal].x, 2);
-  	float diffy = pow(currentPosition->point.y - points[currentGoal].y, 2);
-	float distance = sqrt(diffx + diffy);
-  	
-  	//ROS_INFO("Distance: [%f]", distance);
-	return (distance < 3.0);
+  float diffx = pow(currentPosition->point.x - points[currentGoal].x, 2);
+  float diffy = pow(currentPosition->point.y - points[currentGoal].y, 2);
+  float distance = sqrt(diffx + diffy);
+
+  // ROS_INFO("Distance: [%f]", distance);
+  return (distance < 3.0);
 }
 
-void ObstacleController::convertCommand(const geometry_msgs::Twist::ConstPtr& motionIn){
+void ObstacleController::convertCommand(
+    const geometry_msgs::Twist::ConstPtr &motionIn) {
   command_data cmd;
-  cmd.motor_level = int(motionIn->linear.x * 10);
-  if(motionIn->linear.x > 0.0 && cmd.motor_level < 4){
-  	cmd.motor_level = 4;
-  } else if (motionIn->linear.x < 0.0 && cmd.motor_level > -10){
-  	cmd.motor_level = -10;  	
-  }
-  
-  cmd.steering_level = int((motionIn->angular.z) * 50);
-  if(cmd.steering_level > 45){
-  	cmd.steering_level = 45;
-  } else if (cmd.steering_level < -45){
-  	cmd.steering_level = -45;
-  }
 
-  if(cmd.steering_level > 4 && cmd.steering_level < 20){
-    cmd.steering_level = 20;
-  } else if (cmd.steering_level < -4 && cmd.steering_level > -20){
-    cmd.steering_level = -20;
+  const float alpha_max = M_PI_2 - std::atan(0.85 / 0.28);
+  const float maxSteeringLeft = 45;
+  const float maxSteeringRight = -45;
+  const float steeringWidth = maxSteeringLeft - maxSteeringRight;
+  const float steeringMitte = maxSteeringRight + steeringWidth / 2;
+
+  cmd.steering_level =
+      steeringMitte + (motionIn->angular.z / alpha_max) * steeringWidth / 2;
+
+  if (cmd.steering_level > 45) {
+    cmd.steering_level = 45;
+  } else if (cmd.steering_level < -45) {
+    cmd.steering_level = -45;
   }
 
+  /*if (us_front > 0.65 && std::abs(cmd.steering_level - steeringMitte) < 10) {
+    cmd.motor_level = motionIn->linear.x * 50;
+  } else*/
+  cmd.motor_level = int(motionIn->linear.x * 20);
 
+  if (motionIn->linear.x > 0 && cmd.motor_level < 5) {
+    cmd.motor_level = 5;
+  } else if (motionIn->linear.x < 0.0 && cmd.motor_level > -10) {
+    cmd.motor_level = -10;
+  }
+
+  ROS_INFO("motionIn->linear.x: %f -> Motor Level: %i us_front: %f steering "
+           "level: %f",
+           motionIn->linear.x, cmd.motor_level, us_front,
+           std::abs(cmd.steering_level - steeringMitte));
+  if (cmd.motor_level > 13)
+    cmd.motor_level = 13;
+  else if (cmd.motor_level < -12)
+    cmd.motor_level = -12;
 
   cmd.header.stamp = ros::Time::now();
   command_pub->publish(cmd);
@@ -136,33 +155,39 @@ void ObstacleController::convertCommand(const geometry_msgs::Twist::ConstPtr& mo
 
 void ObstacleController::run() {
 
-	// Trans
-	try {
+  // Trans
+  try {
     tf::StampedTransform transform;
-  		geometry_msgs::PointStamped currentPosition;
+    geometry_msgs::PointStamped currentPosition;
 
-    	transformListener.waitForTransform("base_link", "/map", ros::Time(0),
-                                         ros::Duration(0.1));
-    	transformListener.lookupTransform("base_link", "/map", ros::Time(0),
-                                        transform);
+    transformListener.waitForTransform("base_link", "/map", ros::Time(0),
+                                       ros::Duration(0.1));
+    transformListener.lookupTransform("base_link", "/map", ros::Time(0),
+                                      transform);
 
-		geometry_msgs::PointStamped positionInBaseLink, startInBaseLaser;
+    geometry_msgs::PointStamped positionInBaseLink, startInBaseLaser;
 
-		positionInBaseLink.point.x = 0;
-		positionInBaseLink.point.y = 0;
-		positionInBaseLink.header.frame_id = "/base_link";
-		positionInBaseLink.header.stamp = ros::Time(0);
-		transformListener.transformPoint("/map", positionInBaseLink, currentPosition);
-		currentPosition.point.z = 0;
+    positionInBaseLink.point.x = 0;
+    positionInBaseLink.point.y = 0;
+    positionInBaseLink.header.frame_id = "/base_link";
+    positionInBaseLink.header.stamp = ros::Time(0);
+    transformListener.transformPoint("/map", positionInBaseLink,
+                                     currentPosition);
+    currentPosition.point.z = 0;
 
-  		if(isNearToNextGoal(&currentPosition)){
-  			sendNextGoal();
-  		}
-
-    } catch (tf::TransformException ex) {
-      ROS_ERROR("%s", ex.what());
-      return;
+    if (isNearToNextGoal(&currentPosition)) {
+      // sendNextGoal();
     }
+
+  } catch (tf::TransformException ex) {
+    ROS_ERROR("%s", ex.what());
+    return;
+  }
+}
+
+void ObstacleController::sensorDataCallback(
+    const pses_basis::SensorDataConstPtr &msg) {
+  us_front = msg->range_sensor_front;
 }
 
 #endif
