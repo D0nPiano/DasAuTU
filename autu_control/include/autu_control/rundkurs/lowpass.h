2f714812f5325c1d89b9481122e0f659f4910760
Dominik Lorych <asdasd1995@gmx.de>
2017-03-02 11:50:54 +0100

    lowpass Kommentare

--- autu_control/include/autu_control/rundkurs/lowpass.h
+++ autu_control/include/autu_control/rundkurs/lowpass.h
@@ -4,14 +4,26 @@
 #include <cstddef>
 #include <list>
 
+/**
+* @brief implements a abstract lowpass
+* calculates average of given count of last values
+*/
 class Lowpass {
 public:
   Lowpass(size_t maxValues);
+  /**
+  * @brief add one Value and delete last Value if necessary
+  */
   void addValue(float value);
+  /**
+  *@brief get Average
+  */
   float getAverage() const;
 
 private:
+	//list of values
   std::list<float> values;
+  //size of values
   size_t maxValues;
 };
 
