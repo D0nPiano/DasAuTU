e5616199aa1c5db00a85deef342970fc3fb12cb5
nflo <florian.netzer@arcor.de>
2017-03-06 14:17:52 +0100

    pd controller aufgeräumt und kommentiert

--- autu_control/src/rundkurs/pdcontroller.cpp
+++ autu_control/src/rundkurs/pdcontroller.cpp
@@ -28,33 +28,20 @@ void PDController::reset() {
 void PDController::drive(float ldist, bool left) {
   pses_basis::Command cmd;
 
-  // float parallelAngle =  -(90 - laserDetector->getAngleToWallInDeg());
-
-  // ldist = ldist + wallDist / 2.0;
-
-  /* TODO: Get corner, set
-                  drivingCurve = true;
-                  curveBegin = ros::Time::now().toSec();
-                  */
 
   float steerfact = left ? -2.0f : 2.0f;
 
-  // P-Regler, tb = 62s
 
   cmd.motor_level = maxMotorLevel;
-  // TODO k1 & k2 are unused
-  // float k1 = 16;
-  // float k2 = 5;
+
   double t = ros::Time::now().toSec();
   float e = solldist - ldist;
   cmd.steering_level = steerfact * p * (e + (e - e0) * d / (t - t0));
-  // ROS_INFO("parallelAngle:[%f]", parallelAngle);
-  // cmd.steering_level = (solldist*16 - (k1 * ldist + k2 * parallelAngle )) *
-  // steerfact;
+
 
   e0 = e;
   t0 = t;
-
+  //max sttering level
   if (cmd.steering_level > 40)
     cmd.steering_level = 40;
   else if (cmd.steering_level < -40)
